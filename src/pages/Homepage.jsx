b546235a35d00cf4655b6949caa3a562f6a52006
wongc138 <wongc138@uni.coventry.ac.uk>
2025-05-17 22:28:14 +0100

    17-07 CSS Modules-Not Working

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
-      <h1>Wordlwise</h1>
-      <Link to="/pricing">Pricing</Link>
+      <AppNav />
+      <h1 className="test">Wordlwise</h1>
+      <Link to="/app">Go to the App</Link>
     </div>
   );
 }
