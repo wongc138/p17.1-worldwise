738d53268c482365c63cd2a1dd8d7c77e25adc20
wongc138 <wongc138@uni.coventry.ac.uk>
2025-05-17 21:09:23 +0100

    17-05 NavLink

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function Homepage() {
-  return <div>Wordlwise</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Wordlwise</h1>
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
 }
 
 export default Homepage;
