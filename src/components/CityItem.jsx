7824193a1dee3c9daa15abe4b4eea33b75fff0fb
wongc138 <wongc138@uni.coventry.ac.uk>
2025-05-23 22:16:44 +0100

    17-11 add country

---
+++ src/components/CityItem.jsx
@@ -0,0 +1,33 @@
+import styles from "./CityItem.module.css";
+import PropTypes from "prop-types";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
+function CityItem({ city }) {
+  const { cityName, emoji, date } = city;
+
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{emoji}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>({formatDate(date)}) </time>
+      <button className={styles.deleteBtn} type="button">
+        &times;
+      </button>
+    </li>
+  );
+}
+CityItem.propTypes = {
+  city: PropTypes.shape({
+    cityName: PropTypes.string.isRequired,
+    emoji: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default CityItem;
---
+++ src/components/CityList.jsx
@@ -0,0 +1,26 @@
+import CityItem from "./CityItem";
+import styles from "./CityList.module.css";
+import Message from "./Message";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) {
+    return <Spinner />;
+  }
+  if (!cities.length) return <Message message="No cities found" />;
+
+  return (
+    <ul className={styles.CityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+CityList.propTypes = {
+  cities: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
+
+export default CityList;
